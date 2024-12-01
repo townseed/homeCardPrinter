9422de61601b8b7c02f192cf112ac84052c5c3ab
Ethan <townseed@rose-hulman.edu>
2024-12-01 00:23:04 -0500

    added generic image support, verbose mode, and spaces in image names

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 2) Put list of cards in list.txt (expected format is number of copies followed by the name of the image)
   -comments can be added with #
   -set the card render size with 'standard' (2.5 by 3.5 inches) or 'small' (2.25 by 3.25 inches)
+  -expected image format can be set with 'format' followed by the image format, options are restricted to PIL image options
+  -output can be enabled by putting 'verbose' on a line
 4) run makedeck.py
 5) wait (run in a terminal to get a verbose output)
 6) look at the printme.pdf to make sure it rendered properly
--- makedeck.py
+++ makedeck.py
@@ -5,9 +5,11 @@ from PIL import Image
 
 # card = (2.75*inch, 3.75*inch)
 
+verbose = False
 fixedYOffset = 10
 fixedXOffset = 30
 folderString = "images\\"
+imageExtention = ".png"
 c = canvas.Canvas("printme.pdf", letter)
 # with Image.open("Leystone of Being.png") as being:
 cardSize = 0
@@ -24,8 +26,16 @@ for card in deck:
                 cardSize = 1
             elif info[0] == "standard":
                 cardSize = 0
+            elif info[0] == "format":
+                if len(info) < 2:
+                    if verbose:
+                        print("provide an image extention after 'format'")
+                    exit()
+                imageExtention = info[1]
+            elif info[0] == "verbose":
+                verbose = True
             else:
-                cards.append(info[1])
+                cards.append(card.strip()[card.strip().find(' ') + len(info[0] + " "):])
                 quant.append(info[0])
 
 if cardSize == 1:
@@ -36,20 +46,20 @@ else:
     xOffsetConstant = 2.5 * inch
     yOffsetConstant = 3.5 * inch
 
-print("read deck as:")
-for i, card in enumerate(cards, start = 0):
-    print(quant[i] + " x " + card)
+if verbose:
+    print("read deck as:")
+    for i, card in enumerate(cards, start = 0):
+        print(quant[i] + " x " + card)
 
 cardOnPage = 0
-for i, card in enumerate(cards, start = 0):
-    #change x and y offset based on the posistion on the page
-    
-    cardImage = Image.open(folderString + card + ".png")
-    print("attempting to render: "+folderString +card)
+for i, card in enumerate(cards, start = 0):  
+    cardImage = Image.open(folderString + card + imageExtention)
+    if verbose:
+        print("attempting to render: "+folderString +card)
     for x in range(int(quant[i])):
         yOffset = yOffsetConstant * int(cardOnPage/3) + fixedYOffset
         xOffset = xOffsetConstant * (cardOnPage % 3) + fixedXOffset
-        c.drawImage(folderString + card + ".png", xOffset, yOffset, width=xOffsetConstant, height=yOffsetConstant, mask=None)
+        c.drawImage(folderString + card + imageExtention, xOffset, yOffset, width=xOffsetConstant, height=yOffsetConstant, mask=None)
         cardOnPage = cardOnPage + 1
         if cardOnPage == 9:
             cardOnPage = 0
